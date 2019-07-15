da0c4354559888afc3006b9ded023694a630b6e6
FrancoMaxime <franco.maxime@gmail.com>
2019-07-15 10:54:42 +0200

    add more unit test, bug found

--- FIFO_Test/fifo_test.cpp
+++ FIFO_Test/fifo_test.cpp
@@ -92,7 +92,7 @@ int test_one_element(){
     }
     exFIFO.push(0);
     if(exFIFO.size() !=1){
-        std::cout << "Error exFIFO.size()"<< exFIFO.size()<< std::endl;
+        std::cout << "Error exFIFO.size()"<< std::endl;
         return -1;
     }
     if(exFIFO.isEmpty()){
@@ -118,55 +118,62 @@ int test_one_bad_element(){
     }
     return 0;
 }
+
 int test_random_input(){
     FIFO exFIFO;
 	uint8_t displayBuf[50];
-	int test1 = getSize();
-	int test2 = getSize();
-	int test3 = getSize();
-	int test4 = getSize();
-	int test5 = getSize();
-	int test6 = getSize();
-	int test7 = getSize();
-	int test8 = getSize();
-	int test9 = getSize();
-	char ex1[test1];
-	fillTab(ex1, '1', sizeof(ex1));
-	fillFifo(& exFIFO,ex1,sizeof(ex1));
-	char ex2[getSize()];
-	fillTab(ex2, '2', sizeof(ex2));
-	fillFifo(&exFIFO, ex2, sizeof(ex2));
-	char ex3[getSize()];
-	fillTab(ex3, '3', sizeof(ex3));
-	fillFifo(&exFIFO, ex3, sizeof(ex3));
-	char ex4[getSize()];
-	fillTab(ex4, '4', sizeof(ex4));
-	fillFifo(&exFIFO, ex4, sizeof(ex4));
-	char ex5[getSize()];
-	fillTab(ex5, '5', sizeof(ex5));
-	fillFifo(&exFIFO, ex5, sizeof(ex5));
-	char ex6[getSize()];
-	fillTab(ex6, '6', sizeof(ex6));
-	fillFifo(&exFIFO, ex6, sizeof(ex6));
-	char ex7[getSize()];
-	fillTab(ex7, '7', sizeof(ex7));
-	fillFifo(&exFIFO, ex7, sizeof(ex7));
-	char ex8[getSize()];
-	fillTab(ex8, '8', sizeof(ex8));
-	fillFifo(&exFIFO, ex8, sizeof(ex8));
-	char ex9[getSize()];
-	fillTab(ex9, '9', sizeof(ex9));
-	fillFifo(&exFIFO, ex9, sizeof(ex9));
+	int test_size[10];
+    int total_elem =0;
+    std::cout <<"Test exFIFO with random values" << std::endl;
+    for( int i =0; i < 10; i++){
+        test_size[i] = getSize();
+        total_elem += 1 + test_size[i];
+        char ex1[test_size[i]];
+
+        fillTab(ex1, 48+i, sizeof(ex1));
+        fillFifo(& exFIFO,ex1,sizeof(ex1));
+
+        if(exFIFO.size() != total_elem){
+            std::cout << "Error exFIFO.size()"<< std::endl;
+            return -1;
+        }
+    }
 	while(!(exFIFO.isEmpty())) {
         uint8_t test = exFIFO.peek();
-        exFIFO.popString(displayBuf, test);
-        display(displayBuf, test);
+        if(exFIFO.peekString(displayBuf, test) != test){
+            std::cout << "Error exFIFO.peekString()"<< std::endl;
+            return -1;
+        }
+        if(exFIFO.popString(displayBuf, test) != test){
+            std::cout << "Error exFIFO.popString()"<< std::endl;
+            return -1;
+        }
     }
+    return 0;
 }
 
+int test_toomuch_elements(){
+    std::cout <<"Test exFIFO out of bounds" << std::endl;
+    FIFO exFIFO;
+	uint8_t displayBuf[511];
+    exFIFO.push(511);
+    for(int i=0; i < 511; i++){
+        exFIFO.push(i);
+    }
+    if(exFIFO.size() !=512){
+        std::cout << "Error exFIFO.size()"<< std::endl;
+        return -1;
+    }
+    if(exFIFO.push(23)){
+        std::cout << "Error exFIFO.push()"<< std::endl;
+        return -1;
+    }
+    if(exFIFO.peekString(displayBuf,511) != 511){
+        std::cout << "Error exFIFO.peekString()"<< (int)exFIFO.peekString(displayBuf,511) << std::endl;
+        return -1;
+    }
+}
 int main() {
-
-
     if(test_empty_size() !=0)
         return -1;
     if(test_one_element() != 0)
@@ -175,9 +182,12 @@ int main() {
         return -1;
     if(test_random_input()!= 0)
         return -1;
+    if(test_toomuch_elements() != 0)
+        return -1;
 
 	return 0;
 }
+
 
 
 
